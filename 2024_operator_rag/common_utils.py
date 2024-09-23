e8538b83d66ed95bf3e145383ce80daf99053bcd
qinyuqing1 <qinyuqing1@xiaomi.com>
2024-09-23 12:10:36 +0800

    opti cuda

--- 2024_operator_rag/common_utils.py
+++ 2024_operator_rag/common_utils.py
@@ -1,6 +1,7 @@
 #coding:utf-8
 import re
 import time
+import torch
 
 # 引用：https://github.com/GLZ1925/-
 text = "“喂！路明非！你给我站住！”叔叔追了出来，在走廊尽头冲他低吼。路明非实在没时间让他兴师问罪了，只好说：“叔叔我真有事得先走，什么事以后再说！”叔叔可不听他说，跑过来一把抓住他的手：“你小子给我说老实话？是不是在外面惹事了？我看外面都是警车还有流氓，他们都是冲你来的？”“没……没有……”路明非想辩解。“你小子真不是骗我们说上学其实跑日本来混黑道了吧？”叔叔瞪着他。“真不是，这事儿一时没法解释……”叔叔从屁股后面摸出金利来的钱包，打开来夹层里有几张日圆钞票，大概一万多的样子。他把那张万圆大钞塞进路明非手里：“叔叔不知道你惹了什么麻烦，你们年轻人见的世面大，有些事不愿告诉我们大人，我问也没用。我以前也惹过事跑过路，跑路身上千万得有现金！银行卡信用卡跑车都没用！”"
@@ -47,6 +48,8 @@ def timeit(func):
 def get_embedding_tool():
     model_name = "bge-large-zh-v1.5"
     model_kwargs = {'device': 'cpu'}
+    if torch.cuda.is_available():
+        model_kwargs = {'device': 'cuda'}
     encode_kwargs = {'normalize_embeddings': True}
     from langchain_community.embeddings import HuggingFaceBgeEmbeddings
     embedding = HuggingFaceBgeEmbeddings(
